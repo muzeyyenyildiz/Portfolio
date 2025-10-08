58afceab530de98c0f3981b7138f45fe79e180a8
Müzeyyen Yıldız <74767244+muzeyyenyildiz@users.noreply.github.com>
2025-10-08 15:52:19 +0300

    change profile photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import {
     Button,
 } from 'react-bootstrap';
 import Image from 'react-bootstrap/Image';
-import profileImage from '../assets/profile.jpg';
+import profileImage from '../assets/profile2.png';
 import { LanguageContent } from './types';
 
 
@@ -12,14 +12,18 @@ import { LanguageContent } from './types';
 const HeroSection = ({ content }: { content: LanguageContent }) => (
     <header id="home" className="hero-bg-custom text-white text-center py-5 mx-auto"
         style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', paddingTop: '80px' }}>
-        <Container >
+        <Container style={{ textAlign: 'center' }}>
             <h1 className="display-3 fw-bold mb-3">{content.name}</h1>
-            <Image
-                src={profileImage} // <-- Use the imported variable here
-                className="img fw-bolder mb-4"
-                roundedCircle
-                alt="profession"
-            />
+            <div style={{ width: '100%', height: 'auto', alignSelf: 'center' }}>
+                <Image
+                    src={profileImage} // <-- Use the imported variable here
+                    className="img fw-bolder mb-4 text-center"
+                    style={{ maxWidth: '300px' }}
+                    roundedCircle
+                    fluid
+                    alt="profession"
+                />
+            </div>
             <p className="lead fw-bolder mb-4">{content.profession}</p>
             <p className="mx-auto fs-5" style={{ maxWidth: '800px' }}>
                 {content.summary}
